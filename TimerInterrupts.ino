d6e7f91a5027556928ca33d2587cc6e39a1dcb13
Raul Ocampo <raul0ca@gmail.com>
2015-07-10 15:55:15 -0700

    Create TimerInterrupts.ino
    Need to check operation since may be running at half intended rate

---
+++ TimerInterrupts.ino
@@ -0,0 +1,86 @@
+//timer interrupts
+//by Amanda Ghassaei
+//June 2012
+//http://www.instructables.com/id/Arduino-Timer-Interrupts/
+
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+*/
+
+//timer setup for timer0, timer1, and timer2.
+//For arduino uno or any board with ATMEL 328/168.. diecimila, duemilanove, lilypad, nano, mini...
+
+//this code will enable all three arduino timer interrupts.
+//timer0 will interrupt at 62 Hz
+//timer1 will interrupt at 1 Hz
+
+//storage variables
+boolean toggle0 = 0;
+boolean toggle1 = 0;
+
+void setup(){
+  
+  //set pins as outputs
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+
+  cli();//stop interrupts
+  //set timer0 interrupt at 62 Hz
+  TCCR0A = 0;// set entire TCCR2A register to 0
+  TCCR0B = 0;// same for TCCR2B
+  TCNT0  = 0;//initialize counter value to 0
+  // set compare match register for 2khz increments
+  OCR0A = 251;// = (16*10^6) / (64*1024) - 1 (must be <256)
+  // turn on CTC mode
+  TCCR0A |= (1 << WGM01);
+  // Set CS01 and CS00 bits for 64 prescaler
+  TCCR0B |= (1 << CS02) | (1 << CS00);   
+  // enable timer compare interrupt
+  TIMSK0 |= (1 << OCIE0A);
+
+  //set timer1 interrupt at 1Hz
+  TCCR1A = 0;// set entire TCCR1A register to 0
+  TCCR1B = 0;// same for TCCR1B
+  TCNT1  = 0;//initialize counter value to 0
+  // set compare match register for 1hz increments
+  OCR1A = 15624;// = (16*10^6) / (1*1024) - 1 (must be <65536)
+  // turn on CTC mode
+  TCCR1B |= (1 << WGM12);
+  // Set CS12 and CS10 bits for 1024 prescaler
+  TCCR1B |= (1 << CS12) | (1 << CS10);  
+  // enable timer compare interrupt
+  TIMSK1 |= (1 << OCIE1A);
+  sei();//allow interrupts
+
+}//end setup
+
+ISR(TIMER0_COMPA_vect){//timer0 interrupt 2kHz toggles pin 8
+//generates pulse wave of frequency 2kHz/2 = 1kHz (takes two cycles for full wave- toggle high then toggle low)
+  if (toggle0){
+    digitalWrite(8,HIGH);
+    toggle0 = 0;
+  }
+  else{
+    digitalWrite(8,LOW);
+    toggle0 = 1;
+  }
+}
+
+ISR(TIMER1_COMPA_vect){
+  if (toggle1){
+    digitalWrite(9, HIGH);
+    toggle1 = 0;
+  }
+  else{
+    digitalWrite(9, LOW);
+    toggle1 = 1;
+  }
+}
+
+void loop(){
+  //do other things here
+}
